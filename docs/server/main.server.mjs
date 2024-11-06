7b064fd6c562096276b7021ceda38b4d4b01495f
Moaed Alloula <moaed.alloula@invi-aligners.com>
2024-11-06 13:58:02 +0300

    Deploy Angular to Github Pages

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GVOJCZMV.mjs";import"./chunk-NQWAFO4K.mjs";import"./chunk-PO5UK2MG.mjs";import"./chunk-UNVEKRJB.mjs";import"./chunk-HOEHZNDT.mjs";import"./chunk-KC6D4DUH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
